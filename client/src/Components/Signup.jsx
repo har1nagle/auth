d6694d5aba2f4b358f644e777ae63829ba68516b
har1nagle <hnagle.0@yahoo.com>
2024-02-22 11:13:49 +0530

    done signup from client

--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -1,24 +1,53 @@
-import React from 'react'
-import '../App.css'
+import React, { useState } from "react";
+import "../App.css";
+import Axios from "axios";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    Axios.post("http://localhost:3000/auth/signup", {
+      username,
+      email,
+      password,
+    }).then((Response) => {
+      console.log(Response)
+    }).catch(err => {
+      console.log(err)
+    })
+  };
   return (
-    <div className='sign-up-container'>
-      <form className='sign-up-form'>
+    <div className="sign-up-container">
+      <form className="sign-up-form" onSubmit={handleSubmit}>
         <h2>Sign Up</h2>
         <label htmlFor="username">Username:</label>
-        <input type="text" placeholder='Username' />
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
         <label htmlFor="email">Email:</label>
-        <input type="email" placeholder='Email'/>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder='********'/>
+        <input
+          type="password"
+          placeholder="********"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button type='submit'>Sign Up</button>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
